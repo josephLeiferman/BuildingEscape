9688e6dee243067b5acb3ba51c9a9cc841c7ca8d
josephLeiferman <joseph.leiferman@gmail.com>
2020-09-02 21:10:09 -0500

    Refactor door open to be triggered by pawn entering volume trigger

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -33,14 +33,21 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-    // UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
-    // UE_LOG(LogTemp, Warning, TEXT("The yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+    if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+    {
+        OpenDoor(DeltaTime);
+    }
 
 
+}
+
+void UOpenDoor::OpenDoor(float DeltaTime) 
+{
     CurrentYaw = FMath::FInterpTo(CurrentYaw, TargetYaw, DeltaTime, 2);
     FRotator DoorRotation = GetOwner()->GetActorRotation();
     DoorRotation.Yaw = CurrentYaw;
     GetOwner()->SetActorRotation(DoorRotation);
-
 }
+
+
 
--- Source/BuildingEscape/OpenDoor.h
+++ Source/BuildingEscape/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -24,6 +25,8 @@ public:
     // Called every frame
     virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+    void OpenDoor(float DeltaTime);
+
 
 private:
 
@@ -32,4 +35,10 @@ private:
 
     UPROPERTY(EditAnywhere)
     float TargetYaw = -90.f;
+
+    UPROPERTY(EditAnywhere)
+    ATriggerVolume* PressurePlate;
+
+    UPROPERTY(EditAnywhere)
+    AActor* ActorThatOpens;
 };
